4ba45c55422dfa1aba98acf5c7b70917d658ae08
shimizu kenji <shimizu.kenji@noncore.net>
2022-08-14 12:33:41 +0900

    feat: access tokenを保存できるように対応

--- Shared/SettingsView.swift
+++ Shared/SettingsView.swift
@@ -1,11 +1,19 @@
 import SwiftUI
 
 struct SettingsView: View {
-    @State private var name = ""
+    @State private var accessToken = ""
+    
     var body: some View {
         Form {
-            TextField("Name", text: $name)
+            TextField("GitHub Personal Access Token", text: $accessToken, onCommit: {
+                UserDefaults.standard.set(self.accessToken, forKey: "GitHubPersonalAccessToken")
+            })
         }
         .padding(5)
+        .frame(width: 560, height: 160)
+        .onAppear {
+            guard let accessToken = UserDefaults.standard.value(forKey: "GitHubPersonalAccessToken") as? String else { return }
+            self.accessToken = accessToken
+        }
     }
 }
